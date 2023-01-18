be41d70154607583819932226a494391351016f5
Viet Tran <viettran101294@gmail.com>
2023-01-18 17:30:12 -0500

    snake game improve with interval

--- input.js
+++ input.js
@@ -1,14 +1,20 @@
 let connection;
-
+let interval;
+let prevKey;
 const handleUserInput = function(key) {
+  const setIntFunc = (execution) => {
+    clearInterval(interval);
+    interval = setInterval(execution, 50);
+  };
   if (key === '\u0003') process.exit();
-  if (key === "w") connection.write("Move: up");
-  if (key === "s") connection.write("Move: down");
-  if (key === "a") connection.write("Move: left");
-  if (key === "d") connection.write("Move: right");
+  if (key === "w" && prevKey !== "s" && prevKey !== "w") setIntFunc(() => connection.write("Move: up"));
+  if (key === "s" && prevKey !== "w" && prevKey !== "s") setIntFunc(() => connection.write("Move: down"));
+  if (key === "a" && prevKey !== "d" && prevKey !== "a") setIntFunc(() => connection.write("Move: left"));
+  if (key === "d" && prevKey !== "a" && prevKey !== "d") setIntFunc(() => connection.write("Move: right"));
   if (key === "y") connection.write("Say: Sup!");
   if (key === "u") connection.write("Say: War!");
   if (key === "i") connection.write("Say: Battle?");
+  prevKey = key;
 };
 
 const setupInput = function(conn) {
